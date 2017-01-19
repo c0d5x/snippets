257d6d753e48222d8dc723a927ba93f5de4c0862
c0d5x <c0d5x@protonmail.com>
2017-01-19 20:22:22 +0000

    tf working with lc+asg

--- terraform/elb_asg_lc.tf
+++ terraform/elb_asg_lc.tf
@@ -1,4 +1,38 @@
 
+variable "region" {
+    default = "us-west-1"
+}
+
+variable "azs" {
+    default = ["us-west-1b", "us-west-1c"]
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "ami" {
+    default = "ami-14fcaf74"
+}
+
+variable "keyname" {
+    default = "gl-josehidalgo-pub-ssh"
+}
+
+variable "securitygroups" {
+    default = "pocdeploy"
+}
+
+variable "placement" {
+    default ="default"
+}
+
+
+provider "aws" {
+    region = "${var.region}"
+}
+
+
 resource "aws_elb" "elbpocdeploy" {
   name = "elbpocdeploy"
   availability_zones = "${var.azs}"
@@ -22,21 +56,17 @@ resource "aws_elb" "elbpocdeploy" {
   idle_timeout = 400
   connection_draining = true
   connection_draining_timeout = 400
-
-  tags {
-    Name = "pocdeploy"
-  }
 }
 
 resource "aws_launch_configuration" "lcpocdeploy" {
-    name = "lcpocdeploy"
+    name_prefix = "lcpocdeploy"
     image_id = "${var.ami}"
     instance_type = "${var.instance_type}"
     key_name = "${var.keyname}"
-    security_groups = "${securitygroups}"
-    placement_tenancy = "${placement}"
-    tags {
-      Name = "pocdeploy"
+    security_groups = ["${var.securitygroups}"]
+    placement_tenancy = "${var.placement}"
+    lifecycle {
+      create_before_destroy = true
     }
 }
 
@@ -48,8 +78,8 @@ resource "aws_autoscaling_group" "asgpocdeploy" {
     lifecycle {
       create_before_destroy = true
     }
-    availability_zones = "${azs}"
-    tags {
-      Name = "pocdeploy"
-    }
+    min_size = 1
+    max_size = 3
+    desired_capacity = 2
+    availability_zones = "${var.azs}"
 }
